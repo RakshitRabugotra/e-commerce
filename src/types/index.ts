4c8b6b46f6f74a674f1046ff42f1c730a3705b9f
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-09-28 12:15:39 +0530

    Initial Commit

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export interface Product {
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {
+      rate: number,
+      count: number
+  }
+}
